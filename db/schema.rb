3bd327d6756d9a28d9d20ab425240276163a518a
Arun kumar <arunkumar.ry1@gmail.com>
2017-06-01 02:19:29 +0530

    Modifies schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20170530190939) do
     t.string   "website"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
-    t.string   "image"
+    t.string   "avatar"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
